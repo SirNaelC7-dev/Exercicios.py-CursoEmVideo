f5ee39d2d9d25390482e12ab89ad902c5fa3593e
SirNaelC7-dev <77307791+SirNaelC7-dev@users.noreply.github.com>
2021-01-13 20:54:57 -0300

    Add files via upload

---
+++ desafio004.py
@@ -0,0 +1,5 @@
+num1= int(input('primeiro número:'))
+num2= int(input('segundo número:'))
+s=num1+num2
+print('A soma entre',num1,'&',num2,'é igual à:',s )
+print('A soma entre {} & {} é igual à: {}'.format(num1, num2, s))

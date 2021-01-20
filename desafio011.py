e6de33fafcad3a534b52951d05b6c8a129a0a8f6
SirNaelC7-dev <77307791+SirNaelC7-dev@users.noreply.github.com>
2021-01-19 21:22:18 -0300

    Add files via upload

---
+++ desafio011.py
@@ -0,0 +1,11 @@
+nt=int(input('Digite um número: '))
+m1=nt*1
+m2=nt*2
+m3=nt*3
+m4=nt*4
+m5=nt*5
+m6=nt*6
+m7=nt*7
+m8=nt*8
+m9=nt*9
+print('Tabuada:\n {} x 1 = {} \n {} x 2 = {}  \n {} x 3 = {} \n {} x 4 = {} \n {} x 5 = {} \n {} x 6 = {} \n {} x 7 = {} \n {} x 8 = {} \n {} x 9 = {}'.format(nt,m1,nt,m2,nt,m3,nt,m4,nt,m5,nt,m6,nt,m7,nt,m8,nt,m9 ))

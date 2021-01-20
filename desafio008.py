fba1322445cbd83ddaffce33483091f0adc84dab
SirNaelC7-dev <77307791+SirNaelC7-dev@users.noreply.github.com>
2021-01-19 21:01:03 -0300

    Add files via upload

---
+++ desafio008.py
@@ -0,0 +1,5 @@
+num=int(input('Insira um número: '))
+dobro = num*2
+triplo = num*3
+raiz = num**(1/2)
+print('O dobro de {} é {}, seu triplo é {} e sua raíz quadrada é {:3f}'.format(num, dobro, triplo, raiz))

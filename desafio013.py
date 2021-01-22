beeacc93ceedbb4bfba75af5e7188b5c3ddbd7c4
SirNaelC7-dev <77307791+SirNaelC7-dev@users.noreply.github.com>
2021-01-21 22:59:25 -0300

    Add files via upload

---
+++ desafio013.py
@@ -0,0 +1,5 @@
+altura=int(input('Qual a altura da parede? '))
+largura=int(input('Qual a largura da parede? '))
+area=altura*largura
+tinta=area/2
+print('Você tem um total de {} metros quadrados\nVai ser necessário {} litros de tinta para pinta-la'.format(area,tinta ))

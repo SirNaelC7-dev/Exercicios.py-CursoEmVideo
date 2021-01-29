f8b949ec34f6268456bdfa5a7207205d5611cdf9
SirNaelC7-dev <77307791+SirNaelC7-dev@users.noreply.github.com>
2021-01-28 23:40:51 -0300

    Add files via upload

---
+++ desafio017.py
@@ -0,0 +1,4 @@
+import math
+co=int(input('Digite o comprimento do cateto oposto: '))
+ca=int(input('Digite o comprimento do cateto adjacente: '))
+print('O comprimento da hipotenusa é: {:2f}'.format(math.sqrt((co**2+ca**2))))

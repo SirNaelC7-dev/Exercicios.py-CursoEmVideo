c5a525b2dd172da1dcf10d669f7595157b6ce5c5
SirNaelC7-dev <77307791+SirNaelC7-dev@users.noreply.github.com>
2021-01-29 16:10:33 -0300

    Add files via upload

---
+++ desafio018.py
@@ -0,0 +1,6 @@
+import math
+ang=int(input('Digite o ângulo do triângulo: '))
+seno=math.sin(ang)
+cosseno=math.cos(ang)
+tangente=math.tan(ang)
+print('Seno: {:.2f}\nCosseno: {:.2f}\nTangente: {:.2f}'.format(seno, cosseno, tangente))

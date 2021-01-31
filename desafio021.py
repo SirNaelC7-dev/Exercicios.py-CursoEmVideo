7a52053ac3743fcf719039e9186980dc061c1d3f
SirNaelC7-dev <77307791+SirNaelC7-dev@users.noreply.github.com>
2021-01-31 18:02:24 -0300

    Update desafio021.py

--- desafio021.py
+++ desafio021.py
@@ -1,5 +1,5 @@
-import pygame
+import pygame/*Módulo utilizado para jogos ex:trliha sonora*/
 pygame.init()
-pygame.mixer.music.load('ex21.mp3')
+pygame.mixer.music.load('arquivo.mp3')
 pygame.mixer.music.play()
 pygame.event.wait()

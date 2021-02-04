9f3d8b30d5126b59d3edd04e30f44179a2f51770
SirNaelC7-dev <77307791+SirNaelC7-dev@users.noreply.github.com>
2021-02-04 00:07:53 -0300

    Add files via upload

---
+++ desafio024.py
@@ -0,0 +1,5 @@
+cidade=str(input('Em que cidade você nasceu: ')).strip()
+print('A cidade inicia com o nome Santo? {}'.format(cidade[0:5].upper()=='SANTO'))
+
+
+
---
+++ desafio025.py
@@ -0,0 +1,3 @@
+nome=str(input('Digite seu nome completo: ')).strip()
+n=nome.upper()
+print('O nome tem Silva na sua composição? {}'.format('SILVA' in n))

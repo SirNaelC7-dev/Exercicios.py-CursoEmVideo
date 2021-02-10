e2eee316872b490fdb2f3e1ea4a898acf782d128
SirNaelC7-dev <77307791+SirNaelC7-dev@users.noreply.github.com>
2021-02-09 23:09:00 -0300

    Update desafio036.py

--- desafio036.py
+++ desafio036.py
@@ -2,9 +2,9 @@ pcasa=float(input('Digite o preço da casa: '))
 salario=float(input('Digite seu salário: '))
 tpg=int(input('Em quantos anos pretende pagar? '))
 prestacao=pcasa/(tpg*12)
-print('O vslor da prestação = {:.2f}'.format(prestacao))
+print('O valor da prestação = {:.2f}'.format(prestacao))
 if prestacao > ((salario/100)*30)+salario:
-	print('Seu empréstimo for reprovado!')
+	print('Seu empréstimo foi reprovado!')
 else:
 	print('Seu empréstimo será aprovado!')
 

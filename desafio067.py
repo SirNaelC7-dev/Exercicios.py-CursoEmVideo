0cdbfaac4d2d1cbe39019b50e0aeb1f74b9e1f40
SirNaelC7-dev <77307791+SirNaelC7-dev@users.noreply.github.com>
2021-02-27 00:51:27 -0300

    Update desafio067.py

--- desafio067.py
+++ desafio067.py
@@ -3,6 +3,6 @@ while True:
 	n=int(input('Qual tabuada deseja exibir?'))
 	if n < 0:
 		break
-	for i in range(0,10):
+	for i in range(1,10):
 		print('{} x {} = {}'.format(n, i, n*i))
 print('FIM')

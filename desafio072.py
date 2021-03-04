c0b2b421162ada80f66112b14ef363d15cfa8ddd
SirNaelC7-dev <77307791+SirNaelC7-dev@users.noreply.github.com>
2021-03-04 00:45:11 -0300

    Add files via upload

---
+++ desafio072.py
@@ -0,0 +1,3 @@
+nums= ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Quatorze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte')
+ext=int(input('Digite um número entre: (0-20)'))
+print(f'Você digitou o número {nums[ext]}')

0b10424ef2e0d6e874c58d5784d3939d7caaec19
SirNaelC7-dev <77307791+SirNaelC7-dev@users.noreply.github.com>
2021-03-05 22:43:29 -0300

    Update desafio075.py

--- desafio075.py
+++ desafio075.py
@@ -8,7 +8,7 @@ if 3 in num:
 	print(f'O valor 3 apareceu na posição: {num.index(3)+1}')
 else:
 	print('O número 3 não foi digitado')
-print('Os valores pares digitados foram', end'')
+print('Os valores pares digitados foram: ', end='')
 for n in num:
 	if n%2==0:
 		print(n, end='')

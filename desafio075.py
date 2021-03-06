64852af8c884537bebeda6ab0ac6eeb7b3e03263
SirNaelC7-dev <77307791+SirNaelC7-dev@users.noreply.github.com>
2021-03-05 22:42:07 -0300

    Update desafio075.py

--- desafio075.py
+++ desafio075.py
@@ -1,7 +1,14 @@
-nums=[0,0,0,0]
-for a in range(0,len(nums)):
-	nums[a] = int(input('Digite o {}° número: '.format(a+1)))
-
-print(nums) 
-print('O número 9 aparece: {} vezes'.format(nums.count(9)))
-
+num = (int(input('Digite o 1º número: ')),
+       int(input('Digite o 2º número: ')),
+       int(input('Digite o 3º número: ')),
+       int(input('Digite o 4º número: ')))
+print('Você digitou os valores: {}'.format(num))
+print(f'O valor 9 apareceu {num.count(9)} vezes')
+if 3 in num:
+	print(f'O valor 3 apareceu na posição: {num.index(3)+1}')
+else:
+	print('O número 3 não foi digitado')
+print('Os valores pares digitados foram', end'')
+for n in num:
+	if n%2==0:
+		print(n, end='')

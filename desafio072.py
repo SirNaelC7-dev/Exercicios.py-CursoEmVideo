4e9dd7c6d9c3175d86bc67cbaa25023cfc5a1ccb
SirNaelC7-dev <77307791+SirNaelC7-dev@users.noreply.github.com>
2021-03-07 21:22:51 -0300

    Update desafio072.py

--- desafio072.py
+++ desafio072.py
@@ -1,3 +1,11 @@
-nums= ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Quatorze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte')
-ext=int(input('Digite um número entre: (0-20)'))
+nums= ('Zero', 'Um', 'Dois', 'Três', 
+       'Quatro', 'Cinco', 'Seis', 'Sete', 
+       'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 
+       'Treze', 'Quatorze', 'Quinze', 'Dezesseis', 
+       'Dezessete', 'Dezoito', 'Dezenove', 'Vinte')
+while True:
+	ext=int(input('Digite um número entre 0-20: '))
+	if 0<= ext <=20:
+		break
+	print('Número fora da faixa, Tente novamente')
 print(f'Você digitou o número {nums[ext]}')

8a4f7da2347da829aaaea069984202646df00cc2
SirNaelC7-dev <77307791+SirNaelC7-dev@users.noreply.github.com>
2021-03-07 21:44:07 -0300

    Update desafio073.py

--- desafio073.py
+++ desafio073.py
@@ -1,14 +1,9 @@
-teams=('Flamengo', 'Internacional', 'Atlético - MG', 'São Paulo', 'Fluminense', 'Grêmio', 'Palmeiras', 'Santos', 'Athlétcio - PR', 'Bragantino', 'Ceará', 'Corinthians', 'Atlético - GO', 'Bahia', 'Sport', 'Fortaleza', 'Vasco', 'Goiás', 'Coritiba', 'Botafogo')
-print('Os 5 primeiros colocados foram:')
-for c in range(0,5):
-	print(teams[c], end = ' ')
-print('')
-print('=-=' * 19)	
+teams = 'Flamengo', 'Internacional', 'Atlético-MG', 'São Paulo','Fluminense', 'Grêmio', 'Palmeiras', 'Santos', 'Athlétcio-PR', 'Bragantino', 'Ceará', 'Corinthians', 'Atlético-GO', 'Bahia', 'Sport', 'Fortaleza', 'Vasco', 'Goiás', 'Coritiba', 'Botafogo'
 
-print('Os 4 últimos colocados foram:')
-for c in range(len(teams)-4,len(teams)):
-	print(teams[c], end = ' ')
-	
-print('')
-print('=-=' * 19)	
-print(sorted(teams))
+print(f'Colocações Finais do Brasileirão: {teams}')
+print('-='*30)
+print(f'Os 5 primeiros foram {teams[0:5]}')
+print('-='*30)
+print(f'Os 4 últimos colocados foram{teams[-4:]}')
+print('-='*30)
+print(f'Ordem Alfabética {sorted(teams)}')

a5a357426395851dd0fe3df07c9c159a7fa2b611
SirNaelC7-dev <77307791+SirNaelC7-dev@users.noreply.github.com>
2021-03-07 22:07:12 -0300

    Update desafio074.py

--- desafio074.py
+++ desafio074.py
@@ -1,20 +1,19 @@
 from random import randint
-menornum = 0
-maiornum = 0
-alnums = [0, 0 ,0 ,0 ,0]
-
-for a in range(0,5):
-	aleat = randint(0,10)
-	alnums[a] = aleat
+alnums=(randint(0,10), randint(0,10), randint(0,10), randint(0,10), randint(0,10))
 menornum = alnums[0]
-maiornum = alnums[0] 
+maiornum = alnums[0]
 
-for b in range(1, len(alnums)):
-	if menornum > alnums[b]:
-		menornum = alnums[b]
-	if maiornum < alnums[b]:
-		maiornum = alnums[b]
+for n in range(0, len(alnums)):
+	if menornum > alnums[n]:
+		menornum = alnums[n]
+	if maiornum < alnums[n]:
+		maiornum = alnums[n]
 		
 print(f'Os números gerados foram: {alnums}')
-print(f'O maior número gerado foi:{maiornum}')
-print(f'O menor número gerado foi:{menornum} ')
+
+print(f'O maior número gerado foi: {maiornum}')
+print(f'O menor número gerado foi: {menornum} ')
+
+print(f'O maior número gerado foi: {max(alnums)}')
+print(f'O menor número gerado foi: {min(alnums)} ')
+

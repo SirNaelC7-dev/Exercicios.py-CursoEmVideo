9af1d3d8375b31a55bf41d10f02247dcf643b718
SirNaelC7-dev <77307791+SirNaelC7-dev@users.noreply.github.com>
2021-03-20 01:15:59 -0300

    Update desafio086.py

--- desafio086.py
+++ desafio086.py
@@ -1,16 +1,8 @@
-line1 = []
-line2 = []
-line3 = []
-matrix = [line1,
-		  line2, 
-		  line3]
+matrix = [[0, 0, 0],
+	  [0, 0, 0],
+	  [0, 0, 0]]
 for i in range(0, 3):
 	for j in range(0, 3):
-		if i == 0:
-			line1.append(int(input(f'Posição{[i],[j]}: ')))
-		elif i == 1:
-			line2.append(int(input(f'Posição{[i],[j]}: ')))
-		elif i == 2:
-			line3.append(int(input(f'Posição{[i],[j]}: ')))
-
+		 matrix[i][j] = int(input(f'Posição[{i}][{j}]:'))
 print(matrix)	
+

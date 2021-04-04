715b9cdc29c20fa45545b3fe4df36d0c9cd97647
Naelson Fernandes <77307791+SirNaelC7-dev@users.noreply.github.com>
2021-04-03 22:56:53 -0300

    Add files via upload

---
+++ desafio106.py
@@ -0,0 +1,16 @@
+def pyHelp(msg):
+	while True:
+		m = 'SISTEMA DE AJUDA PyHELP'
+		print('_'* len(msg))
+		print(m)
+		print('_'* len(msg))
+		fb = str(input(msg)).strip().lower()
+		print(f'Acessando o manual: {fb}')
+		print(help(fb))
+		if fb == 'fim':
+			break
+		
+		
+
+
+f = pyHelp('Função ou Biblioteca > ')

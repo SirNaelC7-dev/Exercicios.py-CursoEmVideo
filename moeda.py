b1d3a675858bed9163bb535b43dff3273b5d9405
Naelson Fernandes <77307791+SirNaelC7-dev@users.noreply.github.com>
2021-04-07 21:23:45 -0300

    Add files via upload

---
+++ moeda.py
@@ -0,0 +1,18 @@
+def aumentar(preco, taxa):
+	res = preco + (preço * taxa/100)
+	return res
+	
+	
+def diminuir(preço, taxa):
+	res = preço - (preço * taxa/100)
+	return res
+	
+	
+def dobro(preço):
+	res = preço * 2
+	return res
+	
+	
+def metade(preço):
+	res = preço / 2
+	return res 

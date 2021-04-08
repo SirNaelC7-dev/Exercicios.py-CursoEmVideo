9ca768ebd9a7e67482981ab9ead09ada676fd585
Naelson Fernandes <77307791+SirNaelC7-dev@users.noreply.github.com>
2021-04-07 21:21:55 -0300

    Update desafio106.py

--- desafio106.py
+++ desafio106.py
@@ -1,4 +1,13 @@
-import uteis
-num = int(input('Digite um valor: '))
-fat = uteis.fatorial(num)
-print(f'O fatorial de {num} e {fat}')
+def pyHelp(msg):
+	while True:
+		m = 'SISTEMA DE AJUDA PyHELP'
+		print('_'* len(msg))
+		print(m)
+		print('_'* len(msg))
+		fb = str(input(msg)).strip().lower()
+		print(f'Acessando o manual: {fb}')
+		print(help(fb))
+		if fb == 'fim':
+			break
+		
+f = pyHelp('Função ou Biblioteca > ')

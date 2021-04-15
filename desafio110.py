e13af0ab2056701cb6a42197458cffb53927ac7f
Naelson Fernandes <77307791+SirNaelC7-dev@users.noreply.github.com>
2021-04-15 00:07:52 -0300

    Add files via upload

---
+++ desafio110.py
@@ -0,0 +1,10 @@
+import moedaforma
+p = float(input('Digite um valor: '))
+moedaforma.resumo(p)
+
+
+#p = float(input('Digite o preço: R$ '))
+#print(f'A metade de {moedaforma.moedaforma(p)} é {moedaforma.metade(p, True)}')
+#print(f'O dobro de {moedaforma.moedaforma(p)} é {moedaforma.dobro(p, True)}')
+#print(f'Aumentando 30% temos {moedaforma.aumentar(p, 30, True)}')
+#print(f'Reduzindo 25% temos {moedaforma.diminuir(p, 25, True)}')
